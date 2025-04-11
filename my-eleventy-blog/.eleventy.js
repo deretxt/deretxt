e6337dc897b3e8bd450c31746b2b491e58bf3e97
deretxt <txtei.exe@gmail.com>
2025-04-11 23:18:34 +0700

    Update .eleventy.js

--- my-eleventy-blog/.eleventy.js
+++ my-eleventy-blog/.eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+  // Tambahkan koleksi 'posts' dari folder ./posts/*.md
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./posts/*.md");
+  });
+
   return {
     dir: {
       input: ".",
